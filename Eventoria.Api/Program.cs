92a2663ea0230d2a9a8abbba616b76ad6ed77be2
AIFTeam-12 <AIFTeam-12@DESKTOP-T7AEVTI>
2026-02-02 21:59:47 +0300

    Swagger Düzenlemesi yapıldı

--- Eventoria.Api/Program.cs
+++ Eventoria.Api/Program.cs
@@ -30,14 +30,13 @@ using (var scope = app.Services.CreateScope())
 await SuperUserSeeder.SeedAsync(app.Services);
 await RoleSeeder.SeedAsync(app.Services);
 
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eventoria.Api v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eventoria.Api v1");
+});
+
 
 app.UseHttpsRedirection();
 
